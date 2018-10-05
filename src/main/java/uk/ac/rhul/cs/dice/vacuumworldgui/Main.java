52387488a90e67a7425d51befcada05e6f5cc8f8
cloudstrife9999 <Emanuele.Uliana.2016@live.rhul.ac.uk>
2018-10-05 15:18:47 +0100

    Implemented dynamic ports.

--- src/main/java/uk/ac/rhul/cs/dice/vacuumworldgui/Main.java
+++ src/main/java/uk/ac/rhul/cs/dice/vacuumworldgui/Main.java
@@ -1,5 +1,7 @@
 package uk.ac.rhul.cs.dice.vacuumworldgui;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
@@ -12,12 +14,27 @@ import org.json.JSONTokener;
 import uk.ac.rhul.cs.dice.vacuumworldgui.communication.VWControllerManager;
 
 public class Main {
-
+    private static final String HOST = "127.0.0.1";
+    private static final String CONFIG_FILE_PATH = "config.json";
+    
     private Main() {}
 
     public static void main(String[] args) throws IOException {
 	LogUtils.log("The GUI is running.");
-	VWControllerManager manager = new VWControllerManager("127.0.0.1", 13337);
+	String port = getPort();
+	
+	if (!checkPort(port)) {
+	    LogUtils.log("Malformed or illegal details have been provided. Please edit " + CONFIG_FILE_PATH + " and retry.");
+	    
+	    return;
+	}
+	else {
+	    init(port);
+	}
+    }
+
+    private static void init(String port) throws IOException {
+	VWControllerManager manager = new VWControllerManager(HOST, Integer.valueOf(port));
 	manager.setupNetwork();
 	
 	VWGameProperties.getInstance().setManager(manager);
@@ -53,5 +70,46 @@ public class Main {
 	List<String> defaultMind = Arrays.asList(root.getString("agents_default"));
 	
 	return Arrays.asList(agentMinds, userMinds, defaultMind);
+    }
+    
+    private static String getPort() {
+	try {
+	    JSONTokener tokener = new JSONTokener(new FileInputStream(CONFIG_FILE_PATH));
+	    JSONObject root = new JSONObject(tokener);
+	    
+	    return root.getString("controller_port");
+	}
+	catch(FileNotFoundException e) {
+	    LogUtils.fakeLog(e);
+	    LogUtils.log(CONFIG_FILE_PATH + " was not found.");
+	    
+	    return null;
+	}
+	catch(Exception e) {
+	    LogUtils.fakeLog(e);
+	    
+	    return null;
+	}
+    }
+    
+    private static boolean checkPort(String port) {
+	if (port == null) {
+	    return false;
+	}
+
+	return testPort(port);
+    }
+
+    private static boolean testPort(String portRepresentation) {
+	try {
+	    int port = Integer.parseInt(portRepresentation);
+
+	    return port > 0 && port < 65536;
+	}
+	catch (NumberFormatException e) {
+	    LogUtils.fakeLog(e);
+
+	    return false;
+	}
     }
 }
