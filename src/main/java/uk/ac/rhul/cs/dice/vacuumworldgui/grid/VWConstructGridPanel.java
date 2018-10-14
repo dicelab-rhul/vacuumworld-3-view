3cbc15835c8762f76d736d3dad0df33985c1d078
cloudstrife9999 <Emanuele.Uliana.2016@live.rhul.ac.uk>
2018-10-14 17:29:08 +0100

    Bugfix.

--- src/main/java/uk/ac/rhul/cs/dice/vacuumworldgui/grid/VWConstructGridPanel.java
+++ src/main/java/uk/ac/rhul/cs/dice/vacuumworldgui/grid/VWConstructGridPanel.java
@@ -3,6 +3,7 @@ package uk.ac.rhul.cs.dice.vacuumworldgui.grid;
 import java.awt.Component;
 
 import uk.ac.rhul.cs.dice.vacuumworldgui.Coordinates;
+import uk.ac.rhul.cs.dice.vacuumworldgui.VWGameProperties;
 
 public class VWConstructGridPanel extends VWAbstractGridPanel {
     
@@ -15,7 +16,7 @@ public class VWConstructGridPanel extends VWAbstractGridPanel {
 	    for(int j = 0; j < getGridSize(); j++) {
 		Coordinates coordinates = new Coordinates(i, j);
 		getState().addEmptyLocationFromView(coordinates);
-		VWLocationPanel panel = new VWLocationPanel(getParent(), getState(), "/res/imgs/locations/white_square.png", i, j);
+		VWLocationPanel panel = new VWLocationPanel(getParent(), getState(), VWGameProperties.getInstance().getWhiteLocationPath(), i, j);
 		
 		getGrid().add(panel.getLocationPanel(), panel.getConstraints());
 	    }
